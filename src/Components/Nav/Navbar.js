150a591553ec535ce2e196f0df1e4143c3a0cff7
Jode Shibli <jodeshibli@gmail.com>
2025-02-24 23:37:46 +0200

    Save changes before switching branch

---
+++ src/Components/Nav/Navbar.js
@@ -0,0 +1,49 @@
+import React from "react"
+import {Navbar , Container , NavDropdown , Collapse , Nav} from 'react-bootstrap';
+import './Nav.css';
+import logo from '../../Assets/logo.png';
+
+const Navbars = ()=>{
+    return(
+        <Navbar expand="lg" className="bg-body-tertiary">
+        <Container>
+          <Navbar.Brand>
+            <img src={logo} title="logo" />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link className="active">Home</Nav.Link>
+              <NavDropdown title="Pages" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">About Us</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.1">Our Team</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.1">FAQ's</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.1">Booking</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.1">Error 404</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.1">Login / Register</NavDropdown.Item>
+              </NavDropdown>
+              <NavDropdown title="Services" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Service</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.1">Service Details</NavDropdown.Item>
+              </NavDropdown>
+              <NavDropdown title="Blog" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Blogs</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.1">Blog Details</NavDropdown.Item>
+              </NavDropdown>
+              <Nav.Link>Contact us</Nav.Link>
+              <Nav.Link>search</Nav.Link>
+              <Nav.Link>icon + 123456789</Nav.Link>
+              <Nav.Link>
+                <button>contact us <span> > </span></button>
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    )
+}
+
+
+
+
+export default Navbars;
