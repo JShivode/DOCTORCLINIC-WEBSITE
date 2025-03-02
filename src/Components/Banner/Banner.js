3eb304ea9c22115bbafe9c026092e9655310ffa1
Jode Shibli <jodeshibli@gmail.com>
2025-03-02 13:32:51 +0200

    update contact.js

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -5,18 +5,18 @@ import { Link } from "react-router-dom";
 import './Banner.css';
 
 
-const Banner = ()=>{
+const Banner = (props)=>{
     return(
         <section className="banner">
             <div className="container">
                 <div className="row">
                     <div className="col-lg-12 col-md-12">
-                        <h2>Contact us</h2>
+                        <h2>{props.title}</h2>
                         <li>
                             <Link to="home">
                                 <FontAwesomeIcon icon={faHome}/> home
                             </Link>
-                            / contact us
+                            / {props.smtitle}
                         </li>
                     </div>
                 </div>
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -4,7 +4,15 @@ import Banner from "../Components/Banner/Banner";
 const Contact =()=>{
     return(
         <>
-            <Banner/>
+            <Banner title="Contact" smtitle="contact"/>
+            <section className="contact-us">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-lg-6 col-md-6"></div>
+                        <div className="col-lg-6 col-md-6"></div>
+                    </div>
+                </div>
+            </section>
         </>
     )
 }
