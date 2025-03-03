e6235df002085f055723b17bfb153df6dd0bc2b1
Jode Shibli <jodeshibli@gmail.com>
2025-03-03 16:18:45 +0200

    update FormContact.js

--- src/pages/FormContact.js
+++ src/pages/FormContact.js
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 
 const FormContact =()=>{
+
+    const [name , setname]= useState("");
+    const [show , setshow]= useState(true);
+
+    const changename = (event)=>{
+        setname(event.target.value)
+    }
+
+    const FormHandler =(event)=>{
+        event.preventDefault();
+        // validation here:
+        if(name.length==""){
+            setshow(false);
+            return;
+        }
+        console.log(name);
+        setshow(true);
+    }
+
+
     return(
-        <form>
+        <form onSubmit={FormHandler}>
             <div className="form-control">
-                <input placeholder="Your Name"/>
+                <input placeholder="Your Name" onChange={changename}/>
+                {!show && <p>not empty</p>}
             </div>
             <div className="form-control">
                 <input placeholder="Your Email"/>
